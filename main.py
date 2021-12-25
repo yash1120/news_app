3f6e9b838e82ccdde0132c49cce89a67f4f7264b
Your Name <you@example.com>
2021-12-25 15:50:26 +0530

    commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask , render_template , redirect 
 from dotenv import load_dotenv
+import flask
 import requests 
 import os
 
@@ -8,9 +9,30 @@ load_dotenv()
 API_KEY = os.getenv("NEWS_API_KEY")
 
 TOP_HEADLINES = f"https://newsapi.org/v2/top-headlines?country=in&apiKey={API_KEY}"
-EVERYTHING    = "https://newsapi.org/v2/everything"
+EVERYTHING    = "https://newsapi.org/v2/everything&apiKey={API_KEY}"
 
-news = requests.get(TOP_HEADLINES).json()
-articles = news['articles']
-for article in articles :
-    print(article['title'])
+# news = requests.get(TOP_HEADLINES).json()
+# articles = news['articles']
+# for article in articles :
+#     print(article['title'])
+
+app = Flask(__name__)
+
+CATEGORIES = ('general', 'sports', 'business', 'entertainment', 'health',
+              'science', 'technology')
+@app.route("/")
+def root():
+    news = requests.get(TOP_HEADLINES).json()
+    articles = news['articles']
+    return render_template("index.html",articles=articles)
+
+@app.route("/category/<string:category>")
+def category(category):
+    news = requests.get(TOP_HEADLINES)
+
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        debug=True
+    )
